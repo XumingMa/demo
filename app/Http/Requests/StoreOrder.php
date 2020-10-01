bb879f3fea0a97d2bbc42d31dc295e27e541c446
Xuming <xuming.ma.sr@gmail.com>
2020-10-01 10:04:11 -0400

    fix server nullable

--- app/Http/Requests/StoreOrder.php
+++ app/Http/Requests/StoreOrder.php
@@ -25,15 +25,15 @@ class StoreOrder extends FormRequest
     {
         $today = date('m/d/Y');
         return [
-            'first_name' => 'bail|required|alpha_num|max:32|min:1',
-            'last_name' => 'bail|alpha_num|max:32|min:1',
-            'email' => 'max:80|email',
+            'first_name' => 'required|alpha_num|max:32|min:1',
+            'last_name' => 'nullable|alpha_num|max:32|min:1',
+            'email' => 'nullable|max:80|email',
             'phone' => 'required|max:20|regex:/\+[0-9]{1,2}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}/',
             'schedule_date' => 'required|date|after_or_equal:'.$today,
             'street_address' => 'required|max:200',
             'city' => 'required|max:100',
             'province' => 'required|max:100',
-            'postal' => 'max:10|regex:/[A-Za-z][0-9][A-Za-z] [0-9][A-Za-z][0-9]/',
+            'postal' => 'nullable|max:10|regex:/[A-Za-z][0-9][A-Za-z] [0-9][A-Za-z][0-9]/',
             'country' => 'required|max:100',
         ];
     }
